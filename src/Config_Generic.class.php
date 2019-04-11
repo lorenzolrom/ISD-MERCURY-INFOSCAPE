03af0f3c1abae01778b33dc2c1e2d83848fa19dd
Lorenzo L. Romero <lorenzolrom@gmail.com>
2019-04-10 23:17:55 -0400

    Adding generic config file

---
+++ src/Config_Generic.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * LLR Technologies & Associated Services
+ * Information Systems Development
+ *
+ * Mercury Merlot
+ *
+ * User: lromero
+ * Date: 4/07/2019
+ * Time: 3:21 PM
+ */
+
+
+abstract class Config_Generic
+{
+    const OPTIONS = array(
+        'appName' => 'Merlot',
+
+        'baseURL' => 'https://your.domain',
+        'baseURI' => '/',
+
+        'cookieName' => 'MERLOT',
+
+        'icURL' => 'https://infocentral.url/',
+        'icSecret' => 'INFOCENTRAL_SECRET'
+    );
+}
